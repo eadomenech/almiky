382c93d82eef6eaa2e9fa2da37843b95a19700bc
Yenner Díaz <ydiazn@udg.co.cu>
2020-01-29 17:58:15 -0500

    Additive noise steganalisys metric has been documented
     On branch master
     Changes to be committed:
    	modified:   almiky/steganalysis/additive_noise/metrics.py

--- almiky/steganalysis/additive_noise/metrics.py
+++ almiky/steganalysis/additive_noise/metrics.py
@@ -3,7 +3,13 @@ import numpy as np
 from scipy import ndimage
 from scipy.fftpack import dct
 
+
 class HCFCOM:
+    '''
+    Center of mass (COM) of histogram characteristic function (HCF) of
+    color images. HCF is a representation of the image histogram in
+    the frequency domain
+    '''
 
     def histogram(self, image):
         '''
@@ -15,15 +21,22 @@ class HCFCOM:
         N is bean of histogram
         '''
 
-        red_histogram = cv2.calcHist([image],[0],None,[256],[0,256]).reshape(-1)
-        green_histogram = cv2.calcHist([image],[1],None,[256],[0,256]).reshape(-1)
-        blue_histogram = cv2.calcHist([image],[2],None,[256],[0,256]).reshape(-1)
+        red_histogram = cv2.calcHist(
+            [image], [0], None, [256], [0, 256]).reshape(-1)
+        green_histogram = cv2.calcHist(
+            [image], [1], None, [256], [0, 56]).reshape(-1)
+        blue_histogram = cv2.calcHist(
+            [image], [2], None, [256], [0, 256]).reshape(-1)
 
         return np.array([red_histogram, green_histogram, blue_histogram])
 
     def hchf(self, image):
         '''
-        HISTOGRAM CHARACTERISTIC FUNCTION
+        hchf(image) => numpy array: return histogram characteristic function
+        of an color image.
+
+        Array returned has (3, N) shape with one component for each imagen color
+        channels.
         '''
         histogram = self.histogram(image)
         N = int(histogram.shape[1] / 2)
@@ -34,9 +47,9 @@ class HCFCOM:
 
     def __call__(self, image):
         '''
-        center_mass(histogram) => numpy array: Calculate center of mass of
-        histogram characteristic function (a representation of the color image
-        histogram in the frequency domain)
+        center_mass(histogram) => numpy array: return color image`s center of
+        mass. Array returned has (3,) shape with one component for each imagen color
+        channels.
         '''
 
         return np.array([
