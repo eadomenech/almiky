079e4603d0e2b9cf3a308b1ee2de3e3040cfe665
Yenner Diaz <ydiazn@udg.co.cu>
2020-09-04 12:04:56 -0400

    Sets default map for map scanning.
    ScanMapping use a row mayor 8x8 map by default.
    
     Changes to be committed:
    	new file:   almiky/utils/scan/__init__.py
    	new file:   almiky/utils/scan/maps.py
    	renamed:    almiky/utils/scan.py -> almiky/utils/scan/scan.py
    	modified:   almiky/utils/tests/test_scan.py

--- almiky/utils/scan/scan.py
+++ almiky/utils/scan/scan.py
@@ -3,9 +3,11 @@
 
 import numpy as np
 
+from .maps import ROW_MAJOR_8x8
+
 
 class ScanMapping:
-    def __init__(self, block, map):
+    def __init__(self, block, map=ROW_MAJOR_8x8):
         '''
         Initialization
 
@@ -74,15 +76,3 @@ class ScanMapping:
         x, y = self._get_indexes(pos)
 
         self.block[x, y] = value
-
-
-ScanMapping.ZIGZAG_8x8 = [
-    0, 1, 8, 16, 9, 2, 3, 10,
-    17, 24, 32, 25, 18, 11, 4, 5,
-    12, 19, 26, 33, 40, 48, 41, 34,
-    27, 20, 13,  6, 7, 14, 21, 28,
-    35, 42, 49, 56, 57, 50, 43, 36,
-    29, 22, 15, 23, 30, 37, 44, 51,
-    58, 59, 52, 45, 38, 31, 39, 46,
-    53, 60, 61, 54, 47, 55, 62, 63
-]
