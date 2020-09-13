1890db221ac4220056e2ab40ff42fdb2cd7520ce
Yenner Joaquín Díaz Núñez <ydiazn@udg.co.cu>
2020-09-13 12:49:40 -0400

    A objective function weighted aggregation class was added.
     On branch master
     Changes to be committed:
    	new file:   almiky/utils/optimization/__init__.py
    	new file:   almiky/utils/optimization/functions/__init__.py
    	new file:   almiky/utils/optimization/functions/multiple.py
    	new file:   almiky/utils/tests/optimization/__init__.py
    	new file:   almiky/utils/tests/optimization/test_functions.py

---
+++ almiky/utils/tests/optimization/test_functions.py
@@ -0,0 +1,42 @@
+from unittest import TestCase
+from unittest.mock import Mock
+
+from almiky.utils.optimization.functions import multiple
+from almiky.utils.functions import WeightedFunction
+
+class ConventionalWeightedAggregationTest(TestCase):
+    def test_evaluation(self):
+        f = Mock(side_effect=lambda x: x * 2)
+        g = Mock(side_effect=lambda x: x * 3)
+        wf = WeightedFunction(f, 0.2)
+        wg = WeightedFunction(g, 0.8)
+
+        F = multiple.WeightedAggretation((wf, wg))
+        value = F(5)
+
+        f.assert_called_once()
+        g.assert_called_once()
+
+        self.assertEqual(value, 14)
+
+    def test_functions_called_with_same_arguments(self):
+        '''
+        Test that an exception is raised when no
+        functions is passed.
+        '''
+
+        with self.assertRaises(ValueError):
+            multiple.WeightedAggretation([])
+
+    def test_evaluation_without_functions(self):
+        '''
+        Test that an exception is raised when no
+        functions is passed.
+        '''
+        f = Mock(return_value=1)
+        g = Mock(return_value=1)
+        wf = multiple.WeightedAggretation((f, g))
+        wf(5, 6, a=7, b=8)
+
+        f.assert_called_once_with(5, 6, a=7, b=8)
+        g.assert_called_once_with(5, 6, a=7, b=8)
