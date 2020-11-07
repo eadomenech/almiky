44c3c0d83eae9dd35a6ae36378aa9ccdc6cd2813
Yenner Diaz <ydiazn@udg.co.cu>
2020-11-06 19:36:27 -0500

    Fixed bug in gaussian an salt & pepper noises.
     On branch master
     Changes to be committed:
    	modified:   almiky/attacks/noise.py
    	modified:   almiky/attacks/tests/test_noise.py

--- almiky/attacks/tests/test_noise.py
+++ almiky/attacks/tests/test_noise.py
@@ -4,7 +4,7 @@
 import random
 import unittest
 from unittest import TestCase
-from unittest.mock import Mock
+from unittest.mock import Mock, patch
 
 import numpy as np
 
@@ -14,199 +14,104 @@ from almiky.attacks import noise as attacks
 class TestSaltAndPepperNoise(TestCase):
     '''Test for Salt and Pepper noise'''
 
-    def test_default_max_value(self):
+    @patch('numpy.random.choice')
+    @patch('numpy.random.uniform')
+    def test_noise(self, uniform_mock, choice_mock):
         '''Test noise application with default maximun image value (255)'''
 
-        random.uniform = Mock(side_effect=[
-            0.7, 0.02, 0.3, 0.2,
-            0.4, 0.02, 0.5, 0.7,
-            0.2, 0.3, 0.03, 0.3,
-            0.4, 0.06, 0.6, 0.2
+        uniform_mock.return_value = np.array([
+            [0.7, 0.02, 0.3, 0.2],
+            [0.4, 0.02, 0.5, 0.7],
+            [0.2, 0.3, 0.03, 0.3],
+            [0.4, 0.06, 0.6, 0.2]
         ])
 
-        random.randint = Mock(side_effect=[
-            0, 1, 0, 1
-        ])
-
-        data = np.random.rand(4, 4)
-        noisy = np.copy(data)
-        noisy[1, 1] = noisy[3, 1] = 255
-        noisy[0, 1] = noisy[2, 2] = 0
-
-        np.testing.assert_almost_equal(
-            attacks.salt_paper_noise(data, density=0.1), noisy)
-
-    def test_custom_max_value(self):
-        '''Test noise application with a custom maximun image value'''
-
-        random.uniform = Mock(side_effect=[
-            0.7, 0.02, 0.1, 0.2,
-            0.4, 0.02, 0.5, 0.7,
-            0.2, 0.3, 0.03, 0.3,
-            0.4, 0.06, 0.6, 0.2
-        ])
-
-        random.randint = Mock(side_effect=[
-            0, 1, 0, 1
+        choice_mock.return_value = np.array([
+            [-255, 255, -255, 255],
+            [-255, 255, -255, 255],
+            [-255, 255, -255, 255],
+            [-255, -255, -255, 255]
         ])
 
         data = np.random.rand(4, 4)
         noisy = np.copy(data)
-        noisy[1, 1] = noisy[3, 1] = 512
-        noisy[0, 1] = noisy[2, 2] = 0
+        noisy[1, 1] = noisy[0, 1] = 255
+        noisy[3, 1] = noisy[2, 2] = 0
 
         np.testing.assert_almost_equal(
-            attacks.salt_paper_noise(data, density=0.1, max_value=512), noisy)
+            attacks.salt_pepper_noise(data, density=0.1), noisy)
 
 
 class TestGaussianNoise(TestCase):
     '''Test for gaussian noise'''
 
-    def test_random_calls(self):
-        '''Test number and arguments of random function calls'''
-
-        random.uniform = Mock(return_value=0.05)
-        random.gauss = Mock(return_value=0.05)
-
-        attacks.gaussian_noise(np.random.rand(4, 4), density=0.1)
-
-        expected_uniform_calls = [((0, 1),)] * 16
-        self.assertEqual(
-            random.uniform.call_args_list, expected_uniform_calls)
-        expected_gauss_calls = [((0, 0.5),)] * 16
-        self.assertListEqual(
-            random.gauss.call_args_list, expected_gauss_calls)
-
-    def test_random_calls_with_custom_arguments(self):
-        '''Test number and arguments of random function calls'''
-
-        random.uniform = Mock(return_value=0.05)
-        random.gauss = Mock(return_value=0.05)
-
-        attacks.gaussian_noise(
-            np.random.rand(4, 4), density=0.1, mu=1, sigma=10)
-
-        expected_gauss_calls = [((1, 10),)] * 16
-        self.assertListEqual(
-            random.gauss.call_args_list, expected_gauss_calls)
-
-    def test_image_min_value_exceded(self):
+    @patch('numpy.std')
+    @patch('numpy.random.normal')
+    def test_default_max_value(self, gaussian_mock, std_mock):
         '''
-        Test noise application when altered values excceds
-        minimun image value
+        Test noise addition when default max intensity is used (255)'
         '''
+        img_std = 54.5
+        percent_noise = 0.1
+        gaussian_mock.return_value = np.array([
+            [4.0, -16.0, 25],
+            [-10.6, 8.7, 4.8],
+            [-10.6, 198.7, 4.8],
+        ])
+        std_mock.return_value = img_std
 
-        random.uniform = Mock(side_effect=[
-            0.7, 0.02, 0.3, 0.2,
-            0.4, 0.02, 0.5, 0.7,
-            0.2, 0.3, 0.03, 0.3,
-            0.4, 0.06, 0.6, 0.2
+        data = np.array([
+            [127, 12, 250],
+            [14, 28, 155],
+            [14, 8, 155],
         ])
 
-        random.gauss = Mock(side_effect=[
-            -18.5, -25.6, -19.4, -30.1
+        noisy = np.array([
+            [131, 0, 255],
+            [3, 36, 159],
+            [3, 206, 159],
         ])
 
-        data = np.random.rand(4, 4)
-        noisy = np.copy(data)
+        ouput = attacks.gaussian_noise(data, percent_noise=percent_noise)
 
-        data[1, 1] = data[3, 1] = 21
-        data[0, 1] = data[2, 2] = 17
+        gaussian_mock.called_with(0, img_std * percent_noise, data.shape)
+        std_mock.called_once_with(data)
+        np.testing.assert_almost_equal(ouput, noisy)
 
-        noisy[1, 1] = noisy[3, 1] = 0
-        noisy[0, 1] = noisy[2, 2] = 0
-
-        np.testing.assert_almost_equal(
-            attacks.gaussian_noise(data, density=0.1), noisy)
-
-    def test_image_max_value_exceded(self):
+    @patch('numpy.std')
+    @patch('numpy.random.normal')
+    def test_custom_max_value(self, gaussian_mock, std_mock):
         '''
-        Test noise application when altered values
-        excceds maximun image value
+        Test noise addition when custom max intensity is used'
         '''
+        img_std = 54.5
+        percent_noise = 0.1
+        gaussian_mock.return_value = np.array([
+            [4.0, -16.0, 25],
+            [-10.6, 8.7, 4.8],
+            [-10.6, 198.7, 4.8],
+        ])
+        std_mock.return_value = img_std
 
-        random.uniform = Mock(side_effect=[
-            0.7, 0.02, 0.3, 0.2,
-            0.4, 0.02, 0.5, 0.7,
-            0.2, 0.3, 0.03, 0.3,
-            0.4, 0.06, 0.6, 0.2
+        data = np.array([
+            [127, 12, 180],
+            [14, 28, 155],
+            [14, 8, 155],
         ])
 
-        random.gauss = Mock(side_effect=[
-            4.0, 6.0, 10.6, 8.7
+        noisy = np.array([
+            [131, 0, 200],
+            [3, 36, 159],
+            [3, 200, 159],
         ])
 
-        data = np.random.rand(4, 4)
-        noisy = np.copy(data)
+        ouput = attacks.gaussian_noise(
+            data, percent_noise=percent_noise, max_value=200)
 
-        data[1, 1] = data[3, 1] = 250
-        data[0, 1] = data[2, 2] = 252
-
-        noisy[1, 1] = noisy[3, 1] = 255
-        noisy[0, 1] = noisy[2, 2] = 255
-
-        np.testing.assert_almost_equal(
-            attacks.gaussian_noise(data, density=0.1), noisy)
-
-
-    def test_image_custom_max_value_exceded(self):
-        '''
-        Test noise application when altered values excceds
-        custom maximun image value'
-        '''
-
-        random.uniform = Mock(side_effect=[
-            0.7, 0.02, 0.3, 0.2,
-            0.4, 0.02, 0.5, 0.7,
-            0.2, 0.3, 0.03, 0.3,
-            0.4, 0.06, 0.6, 0.2
-        ])
-
-        random.gauss = Mock(side_effect=[
-            4.0, 6.0, 10.6, 8.7
-        ])
-
-        data = np.random.rand(4, 4)
-        noisy = np.copy(data)
-
-        data[1, 1] = data[3, 1] = 65530
-        data[0, 1] = data[2, 2] = 65537
-
-        noisy[1, 1] = noisy[3, 1] = 65535
-        noisy[0, 1] = noisy[2, 2] = 65535
-
-        np.testing.assert_almost_equal(
-            attacks.gaussian_noise(data, density=0.1, max_value=65535), noisy)
-
-    def test_noise_addition(self):
-        '''
-        Test noise addition'
-        '''
-
-        random.uniform = Mock(side_effect=[
-            0.7, 0.02, 0.3, 0.2,
-            0.4, 0.02, 0.5, 0.7,
-            0.2, 0.3, 0.03, 0.3,
-            0.4, 0.06, 0.6, 0.2
-        ])
-
-        random.gauss = Mock(side_effect=[
-            4.0, -6.0, -10.6, 8.7
-        ])
-
-        data = np.random.rand(4, 4)
-        noisy = np.copy(data)
-
-        data[1, 1] = data[3, 1] = 127
-        data[0, 1] = data[2, 2] = 132
-
-        noisy[1, 1], noisy[3, 1] = 121, 136
-        noisy[0, 1], noisy[2, 2] = 136, 121
-
-        np.testing.assert_almost_equal(
-            attacks.gaussian_noise(data, density=0.1), noisy)
+        gaussian_mock.called_with(0, img_std * percent_noise, data.shape)
+        std_mock.called_once_with(data)
+        np.testing.assert_almost_equal(ouput, noisy)
 
 
 if __name__ == '__main__':
     unittest.main()
-
