c81170baf316da6f8bc6b8784a383972b9fc665d
Ernesto Avila Domenech <eadomenech@gmail.com>
2020-11-19 16:56:57 -0500

    Specifying versions of dependencies

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
 from setuptools import setup
 
-setup(name='almiky',
-      version='0.1',
-      description='Orthogonal moments library',
+setup(
+    name='almiky',
+    version='0.1',
+    description='Python library for data hiding in images',
       url='https://gitlab.udg.co.cu/Investigacion/almiky',
-      author='Yenner Diaz, Anier Soria',
+      author='Yenner J. Diaz-Nuñez, Anier Soria-Lorente, Ernesto Avila-Domenech',
       author_email='yennerdiaz@gmail.com',
       license='LGPL3',
       packages=['almiky'],
       install_requires=[
-          'numpy',
-          'scipy', 
-          'mpmath',
-          'opencv-python',
-          'mpmath'
+          'numpy==1.19.4',
+          'scipy==1.5.4', 
+          'mpmath==1.1.0',
+          'opencv-python==4.4.0.46'
       ],
       zip_safe=False)
