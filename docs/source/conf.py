726c8a29d6cc54420fbc8733b7457bc35d8b6156
Yenner Joaquín Díaz Núñez <ydiazn@udg.co.cu>
2021-02-17 12:06:08 -0500

    Using unittest.mock in sphinx settings
     On branch master
     Changes to be committed:
    	modified:   docs/source/conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,11 +12,13 @@
 #
 import os
 import sys
-import mock
+from unittest import mock
+
 sys.path.insert(0, os.path.abspath('../..'))
 MOCK_MODULES = [
     'numpy', 'scipy', 'matplotlib', 'matplotlib.pyplot', 'mpmath',
     'scipy.interpolate', 'cv2', 'scipy.spatial', 'imageio']
+
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = mock.Mock()
 
